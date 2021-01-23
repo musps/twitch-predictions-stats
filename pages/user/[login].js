958e66e29df48b85c8dd59de897f05a81a99be8d
Sercan YILDIZ <ysercanz@gmail.com>
2021-01-23 22:12:07 +0100

    Remove unsued packages

--- pages/user/[login].js
+++ pages/user/[login].js
@@ -1,6 +1,5 @@
 import { Fragment } from 'react'
 import { useRouter } from 'next/router'
-import { stringify } from 'query-string'
 import { useQuery, gql } from '@apollo/client'
 import { Channel as ChannelFragments } from 'lib/fragments'
 import Page from 'components/Page'
@@ -44,15 +43,15 @@ const parseFilter = (value = '', defautlValue = 'LATEST') => {
 
 function UserPage() {
   const router = useRouter()
-  const { login, filter } = router.query
-  const filterTag = parseFilter(filter)
+  const { login } = router.query
+  const filter = parseFilter(router.query.filter)
 
   const { loading, error, data, fetchMore, variables, refetch } = useQuery(
     PREDICTIONS_QUERY,
     {
       variables: {
         login: login,
-        filter: filterTag,
+        filter,
         page: 1,
       },
       skip: !login,
@@ -89,55 +88,65 @@ function UserPage() {
   }
 
   const loadFilter = (event) => {
-    const queryParams = stringify({
-      filter: event.target.value,
-    })
+    const query = {
+      filter: parseFilter(event.target.value),
+    }
 
-    router.push(`/user/${login}?${queryParams}`, undefined, {
-      shallow: true,
-    })
+    router.push(
+      { pathname: '/user/[login]', query },
+      { pathname: `/user/${login}`, query },
+      { shallow: true }
+    )
   }
 
   const { channel } = data || {}
 
+  const renderData = () => {
+    if (loading) {
+      return <UserLoading />
+    }
+    if (error) {
+      return <UserError />
+    }
+    if (channel) {
+      return (
+        <Fragment>
+          <div className="flex items-center justify-between">
+            <h1 className="text-xl">Predictions</h1>
+
+            <label labelfor="filter-predictions" className="block">
+              <select
+                aria-label="Filter predictions by criteria"
+                name="filter-predictions"
+                onChange={loadFilter}
+                value={filter}
+                className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-800 dark:bg-gray-800">
+                <option value="LATEST">Latest</option>
+                <option value="OLDEST">Oldest</option>
+              </select>
+            </label>
+          </div>
+
+          <PredictionNodes nodes={channel?.predictions?.nodes} />
+
+          {channel?.predictions?.hasNextPage && (
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-3 bg-gray-200 rounded-md shadow hover:bg-opacity-20 dark:bg-gray-800"
+              onClick={loadMore}>
+              Load more
+            </button>
+          )}
+        </Fragment>
+      )
+    }
+
+    return <UserNotFound />
+  }
+
   return (
     <Page title={login}>
-      <Channel login={login}>
-        {loading && <UserLoading />}
-        {error && <UserError />}
-        {!loading && !error && !channel ? (
-          <UserNotFound />
-        ) : (
-          <Fragment>
-            <div className="flex items-center justify-between">
-              <h1 className="text-xl">Predictions</h1>
-
-              <label labelfor="filter-predictions" className="block">
-                <select
-                  aria-label="Filter predictions by criteria"
-                  name="filter-predictions"
-                  onChange={loadFilter}
-                  value={filterTag}
-                  className="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-800 dark:bg-gray-800">
-                  <option value="LATEST">Latest</option>
-                  <option value="OLDEST">Oldest</option>
-                </select>
-              </label>
-            </div>
-
-            <PredictionNodes nodes={channel?.predictions?.nodes} />
-
-            {channel?.predictions?.hasNextPage && (
-              <button
-                type="button"
-                className="inline-flex items-center justify-center p-3 bg-gray-200 rounded-md shadow hover:bg-opacity-20 dark:bg-gray-800"
-                onClick={loadMore}>
-                Load more
-              </button>
-            )}
-          </Fragment>
-        )}
-      </Channel>
+      <Channel login={login}>{renderData()}</Channel>
     </Page>
   )
 }
